1e81acac81b7c9ce6de29e5fe7196faa05bf1381
Lonny Vberg <lonnyvandenberg@gmail.com>
2022-06-26 18:29:11 -0600

    installed dependencies

---
+++ index.js
@@ -0,0 +1,54 @@
+const Manager = require("./lib/Manager")
+const Intern = require("./lib/Intern")
+const Engineer = require("./lib/Engineer")
+const Employee = require("./lib/Employee")
+const inquirer = require("inquirer");
+
+function createManager() {
+    inquirer.prompt([
+        {
+            type: "input",
+            name: "managerName",
+            message: "What is the team manager's name?",
+            validate: answer => {
+                if (answer !== "") {
+                    return true;
+                }
+                return "Please enter at least one character.";
+            }
+        },
+        {
+            type: "input",
+            name: "managerId",
+            message: "What is the team manager's id?",
+            validate: answer => {
+                if (answer !== "") {
+                    return true;
+                }
+                return "Please enter at least one character.";
+            }
+        },
+        {
+            type: "input",
+            name: "managerEmail",
+            message: "What is the team manager's email?",
+            validate: answer => {
+                if (answer !== "") {
+                    return true;
+                }
+                return "Please enter at least one character.";
+            }
+        },
+        {
+            type: "input",
+            name: "managerOfficeNum",
+            message: "What is the team manager's office number?",
+            validate: answer => {
+                if (answer !== "") {
+                    return true;
+                }
+                return "Please enter at least one character.";
+            }
+        },
+    ]);
+}
