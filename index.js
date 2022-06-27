8e19866db91557af7301615d6e7159219c180a3c
Lonny Vberg <lonnyvandenberg@gmail.com>
2022-06-26 18:39:08 -0600

    added manager functionality

--- index.js
+++ index.js
@@ -4,51 +4,67 @@ const Engineer = require("./lib/Engineer")
 const Employee = require("./lib/Employee")
 const inquirer = require("inquirer");
 
-function createManager() {
-    inquirer.prompt([
-        {
-            type: "input",
-            name: "managerName",
-            message: "What is the team manager's name?",
-            validate: answer => {
-                if (answer !== "") {
-                    return true;
+const employees = []
+
+const init = () => {
+    function createManager() {
+        inquirer.prompt([
+            {
+                type: "input",
+                name: "managerName",
+                message: "What is the team manager's name?",
+                validate: answer => {
+                    if (answer !== "") {
+                        return true;
+                    }
+                    return "Please enter at least one character.";
                 }
-                return "Please enter at least one character.";
-            }
-        },
-        {
-            type: "input",
-            name: "managerId",
-            message: "What is the team manager's id?",
-            validate: answer => {
-                if (answer !== "") {
-                    return true;
+            },
+            {
+                type: "input",
+                name: "managerId",
+                message: "What is the team manager's id?",
+                validate: answer => {
+                    if (answer !== "") {
+                        return true;
+                    }
+                    return "Please enter at least one character.";
                 }
-                return "Please enter at least one character.";
-            }
-        },
-        {
-            type: "input",
-            name: "managerEmail",
-            message: "What is the team manager's email?",
-            validate: answer => {
-                if (answer !== "") {
-                    return true;
+            },
+            {
+                type: "input",
+                name: "managerEmail",
+                message: "What is the team manager's email?",
+                validate: answer => {
+                    if (answer !== "") {
+                        return true;
+                    }
+                    return "Please enter at least one character.";
                 }
-                return "Please enter at least one character.";
-            }
-        },
-        {
-            type: "input",
-            name: "managerOfficeNum",
-            message: "What is the team manager's office number?",
-            validate: answer => {
-                if (answer !== "") {
-                    return true;
+            },
+            {
+                type: "input",
+                name: "managerOfficeNum",
+                message: "What is the team manager's office number?",
+                validate: answer => {
+                    if (answer !== "") {
+                        return true;
+                    }
+                    return "Please enter at least one character.";
                 }
-                return "Please enter at least one character.";
-            }
-        },
-    ]);
+            },
+        ]).then(manager => {
+            const generated = new Manager(
+                manager.managerName,
+                manager.managerId,
+                manager.managerEmail,
+                manager.managerOfficeNum
+            );
+            employees.push(generated);
+            teamGen();
+        });
+    };
 }
+
+
+init();
